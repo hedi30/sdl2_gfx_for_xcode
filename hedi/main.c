6fc11da9907e93777c12d27def943ca9f9100818
hedi <hedihamida98@gmail.com>
2022-10-07 16:59:01 +0100

    Initial Commit

---
+++ hedi/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  hedi
+//
+//  Created by hedi on 7/10/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
